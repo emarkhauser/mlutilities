5e6d12b45422a7f14ca711e430b89ff12c249a56
Erik Markhauser <emarkhauser@yahoo.com>
2018-12-07 15:55:44 -0500

    Update mlutilities.py

--- mlutilities.py
+++ mlutilities.py
@@ -1,7 +1,6 @@
 # Estimate
 def estimate(estimator, parameters, X_train, y_train, X_test):
     from sklearn.model_selection import GridSearchCV
-    from sklearn import metrics
     # Fit the model
     model = GridSearchCV(estimator, parameters, cv=5, iid=False, n_jobs=-1)
     model.fit(X_train, y_train)
@@ -13,5 +12,6 @@ def estimate(estimator, parameters, X_train, y_train, X_test):
     
 # Classification Results
 def classificationResults(predicted, y_test):
+    from sklearn import metrics
     print(metrics.classification_report(y_test, predicted))
     print(metrics.confusion_matrix(y_test, predicted))

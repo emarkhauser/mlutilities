6c4742c7c4a055f8af288078e3894aa656e5dd3f
Erik Markhauser <emarkhauser@yahoo.com>
2018-12-08 08:58:59 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='mlutilities',
+      version='0.1',
+      description='Package common code for more efficient machine learning coding',
+      url='https://github.com/emarkhauser/mlutilities',
+      author='Erik Markhauser',
+      license='GNU GPL',
+      packages=['mlutilities'])

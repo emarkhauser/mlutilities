6a1f8243546ce1c09b6dd8a291318a204c76ab5e
Erik Markhauser <emarkhauser@yahoo.com>
2018-12-08 10:44:09 -0500

    Update mlutilities.py
    added self to functions

--- mlutilities/mlutilities.py
+++ mlutilities/mlutilities.py
@@ -1,5 +1,5 @@
 # Estimate
-def estimate(estimator, parameters, X_train, y_train, X_test):
+def estimate(self.estimator, parameters, X_train, y_train, X_test):
     from sklearn.model_selection import GridSearchCV
     # Fit the model
     model = GridSearchCV(estimator, parameters, cv=5, iid=False, n_jobs=-1)
@@ -11,7 +11,7 @@ def estimate(estimator, parameters, X_train, y_train, X_test):
     return model.predict(X_test)
     
 # Classification Results
-def classificationResults(predicted, y_test):
+def classificationResults(self, predicted, y_test):
     from sklearn import metrics
     print(metrics.classification_report(y_test, predicted))
     print(metrics.confusion_matrix(y_test, predicted))

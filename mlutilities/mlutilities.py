cf966919408dd4ee9a38d99b20b062bd543fa342
Erik Markhauser <emarkhauser@yahoo.com>
2018-12-08 22:48:44 -0500

    Update mlutilities.py
    Better print out of estimate scores for parameters.

--- mlutilities/mlutilities.py
+++ mlutilities/mlutilities.py
@@ -4,9 +4,15 @@ def estimate(estimator, parameters, X_train, y_train, X_test):
     # Fit the model
     model = GridSearchCV(estimator, parameters, cv=5, iid=False, n_jobs=-1)
     model.fit(X_train, y_train)
-    print(model.best_score_)                                  
+    print("BEST SCORE: %r; BEST PARAMETERS: %s" % (model.best_score_, model.best_params_))
     for param_name in sorted(parameters.keys()):
-        print("%s: %r" % (param_name, model.best_params_[param_name]))
+        print("PARAMETER: %s: %r" % (param_name, model.best_params_[param_name]))
+    means = model.cv_results_['mean_test_score']
+    stds = model.cv_results_['std_test_score']
+    params = model.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("Score: %f (%f); Parameters: %r" % (mean, stdev, param))
+
     # Predict using the model
     return model.predict(X_test)
     
